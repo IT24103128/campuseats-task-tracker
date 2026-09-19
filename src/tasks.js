a21944f71590d4e4584df67cefbb4349ba20c4be
Thisal Dilmith <93121062+thisal-d@users.noreply.github.com>
2026-09-19 09:48:18 +0530

    feat: add initial CampusEats task list

---
+++ src/tasks.js
@@ -0,0 +1,8 @@
+"// CapustEats task list" 
+// CampusEats task list
+const tasks = [
+ "Design the menu screen",
+ "Build the orders API",
+ "Add user login",
+];
+console.log(`CampusEats has ${tasks.length} open tasks`);
